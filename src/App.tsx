ae41ea717a2c8aef2e56b8791c4789f6afef8a1f
SolomonLake <lakegh@gmail.com>
2023-12-28 00:35:57 +0100

    Add env and basic gh-pages setup

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,8 @@ const LogoImage = ({
 export const App = () => {
   const [count, setCount] = useState(0);
 
+  console.log("Hello world!", import.meta.env);
+
   return (
     <div className="flex flex-col place-items-center min-h-screen text-center p-8 justify-center">
       <div className="flex">

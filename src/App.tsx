ccab48d0051cac7d396992141f44383568bff146
SolomonLake <lakegh@gmail.com>
2024-01-06 16:00:53 +0100

    Update name to dicey-darts and clean up App

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,7 @@
-import { useState } from "react";
 import compassLogo from "./assets/compass.svg";
-import { twMerge } from "tailwind-merge";
-import { dice2OddsTuple, getOddsCalculator } from "./probs";
 import { Client } from "boardgame.io/react";
 import { DiceyDarts } from "./Game";
 import { Debug } from "boardgame.io/debug";
-
-const LogoImage = ({
-    className,
-    ...props
-}: React.ComponentPropsWithoutRef<"img">) => (
-    <img
-        className={twMerge(
-            "h-24 m-6 transition will-change-contents filter duration-300 hover:drop-shadow-[0_0_32px_#646cffaa]",
-            className,
-        )}
-        {...props}
-    />
-);
 
 const ClientGame = Client({
     game: DiceyDarts,
@@ -28,33 +12,16 @@ const ClientGame = Client({
 });
 
 export const App = () => {
-    const [count, setCount] = useState(0);
-
-    const oddsCalculator = getOddsCalculator(4, 4);
-    (window as any).oddsCalulator = oddsCalculator;
-    const numAllowed = 2;
-    // console.log(oddsCalculator.dice2sums);
-    console.log(oddsCalculator.enumerateOdds(numAllowed));
-    console.log(
-        dice2OddsTuple(oddsCalculator.enumerateOdds(numAllowed)).sort(
-            (a, b) => b[1] - a[1],
-        ),
-    );
-
     return (
-        <div className="flex flex-col place-items-center min-h-screen text-center p-8 justify-center">
-            <div className="flex">
-                <LogoImage
-                    src={compassLogo}
-                    alt="React logo"
-                    className="hover:drop-shadow-[0_0_32px_#61dafbaa] motion-safe:animate-[spin_15s_linear_infinite]"
-                />
+        <div className="flex flex-col place-items-center min-h-screen text-center p-8 justify-between">
+            {/* Header */}
+            <div className="flex flex-row">
+                <div className="flex">
+                    <img src={compassLogo} alt="React logo" className="h-8" />
+                </div>
+                <h1>Dicey Darts</h1>
             </div>
-            <h1>Adventure AI!</h1>
             <div className="p-8">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
                 <p>
                     Edit <code>src/App.tsx</code> and save to test HMR
                 </p>

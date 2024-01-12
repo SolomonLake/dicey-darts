b5081a398ebc641cb136c5e4bf492f4ca578c979
Solomon Lake Giffen-Hunter <lakegh@gmail.com>
2024-01-12 11:47:27 +0100

    try assets link

--- src/components/DiceyDartsBoard/DiceyDartsBoard.tsx
+++ src/components/DiceyDartsBoard/DiceyDartsBoard.tsx
@@ -31,8 +31,8 @@ export const DiceyDartsBoard = (props: MyGameBoardProps) => {
                                         <div className="" key={i}>
                                             <div
                                                 style={{
-                                                    maskImage: `url(${triangleSvg})`,
-                                                    WebkitMaskImage: `url(${triangleSvg})`,
+                                                    maskImage: `url('/assets/triangle.svg')`,
+                                                    WebkitMaskImage: `url('/assets/triangle.svg')`,
                                                     backgroundImage: `url(${triangleSvg})`,
                                                 }}
                                                 className={twMerge(

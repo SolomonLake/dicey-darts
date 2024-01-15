576ee097c566ea02b5718bf7c2c38e7977c63224
SolomonLake <lakegh@gmail.com>
2024-01-15 19:22:44 +0100

    Setup firebase project

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,37 @@ import { Debug } from "boardgame.io/debug";
 import { Local } from "boardgame.io/multiplayer";
 import { DiceyDartsBoard } from "./components/DiceyDartsBoard/DiceyDartsBoard";
 import { DarkModeSwitcher } from "./components/DarkModeSwitcher/DarkModeSwitcher";
+import { initializeApp } from "firebase/app";
+import { getFirestore, getDocs, collection } from "firebase/firestore";
+// import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyArm65a0OQ0uhbOP5yzWL9Kz-dt2ILXHd8",
+    authDomain: "dicey-darts.firebaseapp.com",
+    projectId: "dicey-darts",
+    storageBucket: "dicey-darts.appspot.com",
+    messagingSenderId: "684825016551",
+    appId: "1:684825016551:web:3cae89391b14521b52ca42",
+    measurementId: "G-5R3HS73MGC",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+// Connect to the local emulator if running
+// connectFirestoreEmulator(db, "127.0.0.1", 8080);
+
+const querySnapshot = await getDocs(collection(db, "lobbies"));
+console.log("Lobbies:", querySnapshot.size);
+querySnapshot.forEach((doc) => {
+    console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+});
 
 const ClientGame = Client({
     game: DiceyDarts,
@@ -15,7 +46,7 @@ const ClientGame = Client({
     },
     multiplayer: Local({
         // Enable localStorage cache.
-        // persist: true,
+        persist: true,
     }),
 });
 

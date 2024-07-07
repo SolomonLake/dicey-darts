81314df47c67788444027b71c7bc8fe1baf0e7e5
SolomonLake <lakegh@gmail.com>
2024-07-07 01:49:57 -0500

    progress

--- src/components/GameActions.tsx
+++ src/components/GameActions.tsx
@@ -273,6 +273,24 @@ export const GameActions = (
     const [sideX, setSideX] = useState(0);
     const [sideY, setSideY] = useState(0);
 
+    // const AX = 0;
+    // const AY = 0;
+    // const AZ = 0;
+    // const BX = sideX;
+    // const BY = 0;
+    // const BZ = 0;
+    // const CX = sideX / 2;
+    // const CY = (Math.sqrt(3) / 2) * sideX;
+    // const CZ = 0;
+    // const DX = sideX / 2;
+    // const DY = (Math.sqrt(3) / 6) * sideX;
+    // const DZ = (Math.sqrt(2) / Math.sqrt(3)) * sideX;
+
+    const GX = sideX / 2;
+    const GY = sideY - ((Math.sqrt(3) / 3) * sideX) / 2;
+    const GZ = ((Math.sqrt(2) / Math.sqrt(3)) * sideX) / 4;
+    const tOrigin = `${GX}px ${GY}px ${GZ}px`;
+
     useEffect(() => {
         if (containerRef.current) {
             const { width, height } =
@@ -301,7 +319,7 @@ export const GameActions = (
         <div {...rest}>
             {diceValues.length > 0 && (
                 <div className="grid grid-cols-2 flex-1 gap-3]">
-                    {diceValues.map((diceValue, i) => {
+                    {[1, 2, 3, 4].map((diceValue, i) => {
                         const topHalf = i < diceValues.length / 2;
                         const showSide: {
                             [diceNumber: number]: React.CSSProperties;
@@ -309,78 +327,78 @@ export const GameActions = (
                             1: {},
                             2: {
                                 transform:
-                                    "rotate3d(1, 0, 0, -109deg) rotate3d(0, 0, 1, 60deg)",
-                                // transformOrigin: "top right",
+                                    "rotate3d(1, 0, 0, -110deg) rotate3d(0, 0, 1, 60deg)",
+                                transformOrigin: tOrigin,
                             },
                             3: {
-                                // translate3d(-6%, 39%, 0px)
                                 transform:
-                                    "rotate3d(1, 0, 0, -109deg) rotate3d(0, 0, 1, -60deg)",
+                                    "rotate3d(1, 0, 0, -110deg) rotate3d(0, 0, 1, -60deg) ",
+                                transformOrigin: tOrigin,
                             },
                             4: {
-                                // translate3d(0%, 33%, 0px)
                                 transform:
                                     "rotate3d(1, 0, 0, 70deg) rotate3d(0, 1, 0, 180deg)",
-                                transformOrigin: "0px 30% 0px",
+                                transformOrigin: tOrigin,
                             },
                         };
+                        const zPosition = (-Math.sqrt(6) / 3) * sideX;
                         return (
                             <div
                                 key={i}
                                 className="relative transform transition duration-1000"
-                                ref={containerRef}
+                                {...(i === 0 ? { ref: containerRef } : {})}
                                 style={{
                                     transformStyle: "preserve-3d",
                                     ...showSide[diceValue],
                                 }}
                             >
+                                <div
+                                    {...(i === 0 ? { ref: sideRef } : {})}
+                                    className={twMerge(
+                                        "absolute aspect-[1000/866]",
+                                        dimensionStyle,
+                                    )}
+                                />
                                 {[
                                     {
                                         style: {},
                                     },
                                     {
                                         style: {
-                                            transform:
-                                                "rotate3d(0, 0, 1, -60deg) rotate3d(1, 0, 0, 109deg)",
-                                            transformOrigin: "bottom left",
+                                            // transform: `translate3d(0, 0, ${zPosition}px) rotateY(120deg) rotateX(0deg)`,
+
+                                            transform: `rotate3d(0, 0, 1, -60deg) rotate3d(1, 0, 0, 110deg)`,
+                                            // transformOrigin: "bottom left",
+                                            transformOrigin: tOrigin,
                                         },
                                     },
                                     {
                                         style: {
                                             transform:
-                                                "rotate3d(0, 0, 1, 60deg) rotate3d(1, 0, 0, 109deg)",
-                                            transformOrigin: "bottom right",
-                                            // transform:
-                                            //     "translate3d(15%, 5%, 0px) rotate3d(0, 0, 1, 60deg) rotate3d(1, 0, 0, 109deg)",
+                                                "rotate3d(0, 0, 1, 60deg) rotate3d(1, 0, 0, 110deg)",
+                                            // transformOrigin: "bottom right",
+                                            transformOrigin: tOrigin,
                                         },
                                     },
                                     {
                                         style: {
-                                            transformOrigin: "bottom",
                                             transform:
                                                 "rotate3d(1, 0, 0, 70deg) rotate3d(0, 1, 0, 180deg)",
+                                            // transformOrigin: "bottom",
+                                            transformOrigin: tOrigin,
                                         },
                                     },
                                 ].map((side, sideIndex) => (
                                     <div
-                                        {...(i === 0 && sideIndex === 0
-                                            ? { ref: sideRef }
-                                            : {})}
                                         key={`${i}-${sideIndex}`}
                                         style={{
                                             ...side.style,
                                             // backfaceVisibility: "hidden",
-                                            clipPath:
-                                                "polygon(50% 0%, 0% 100%, 100% 100%)",
                                         }}
-                                        // aspect-[1000/866]
                                         className={twMerge(
-                                            "absolute border-base-200 rounded-lg text-2xl flex justify-center items-center aspect-[1000/866]",
+                                            "absolute border-base-200 mask mask-triangle rounded-md text-2xl flex justify-center items-center aspect-[1000/866]",
                                             topHalf && "self-end",
                                             dimensionStyle,
-                                            dimensionStyle === "w-full"
-                                                ? "top-[25%]"
-                                                : "left-[7%]",
                                             diceBgColor,
                                             diceBgTextColor,
                                         )}
